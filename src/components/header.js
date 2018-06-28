db7d79a80f267b73efcffaee6ba773eab0d2120c
jtkaczyszyn <jtkaczyszyn@elementum.com>
2018-06-28 18:39:11 -0300

    migrate header to styled-components

--- src/components/header.js
+++ src/components/header.js
@@ -1,33 +1,46 @@
-import React from 'react'
-import Link from 'gatsby-link'
+import React from 'react';
+import PropTypes from 'prop-types';
+import Link from 'gatsby-link';
+import styled from 'styled-components';
+
+const HeaderWrapper = styled.div`
+  background: rebeccapurple;
+  margin-bottom: 1.45rem;
+`;
+
+const StyledHeader = styled.div`
+  margin: 0 auto;
+  max-width: 960;
+  padding: 1.45rem 1.0875rem;
+
+  & h1 {
+    margin: 0;
+  }
+`;
+
+const StyledLink = styled(Link)`
+  color: white;
+  text-decoration: none;
+`;
 
 const Header = ({ siteTitle }) => (
-  <div
-    style={{
-      background: 'rebeccapurple',
-      marginBottom: '1.45rem',
-    }}
-  >
-    <div
-      style={{
-        margin: '0 auto',
-        maxWidth: 960,
-        padding: '1.45rem 1.0875rem',
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: 'white',
-            textDecoration: 'none',
-          }}
-        >
+  <HeaderWrapper>
+    <StyledHeader>
+      <h1>
+        <StyledLink to="/">
           {siteTitle}
-        </Link>
+        </StyledLink>
       </h1>
-    </div>
-  </div>
-)
+    </StyledHeader>
+  </HeaderWrapper>
+);
 
-export default Header
+Header.propTypes = {
+  siteTitle: PropTypes.string
+};
+
+Header.defaultProps = {
+  siteTitle: ''
+};
+
+export default Header;
