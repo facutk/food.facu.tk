8de4c612e0473eb5695a3f80cec7af8e612d7141
jtkaczyszyn <jtkaczyszyn@elementum.com>
2018-06-28 12:43:36 -0300

    override helmet data on component

--- src/templates/blogTemplate.js
+++ src/templates/blogTemplate.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Helmet from 'react-helmet'
 
 export default function Template({
   data, // this prop will be injected by the GraphQL query below.
@@ -7,6 +8,12 @@ export default function Template({
   const { frontmatter, html } = markdownRemark;
   return (
     <div className="blog-post-container">
+      <Helmet
+        title={frontmatter.title}
+        meta={[
+          { name: 'description', content: frontmatter.title }
+        ]}
+      />
       <div className="blog-post">
         <h1>{frontmatter.title}</h1>
         <h5>{frontmatter.date}</h5>
